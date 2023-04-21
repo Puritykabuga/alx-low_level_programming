6f570831e9b931550fbd20c5365a4d44303a7cee
purity kabuga <puritykabuga@gmail.com>
2023-04-21 06:19:02 -0700

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints function using printf and the return should be 0
- * returns:(0)
+ * return (0)
  */
-
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
