44d02df42819f30488bf29adbd2aa3e8a0204642
purity kabuga <puritykabuga@gmail.com>
2023-04-23 23:45:31 -0700

    print line using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -writes a program that prints a line using function puts
- * return: 0 - when successful return 0
+ * main - writes a program that prints a line using function puts
+ * Return: 0 - when successful return 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
