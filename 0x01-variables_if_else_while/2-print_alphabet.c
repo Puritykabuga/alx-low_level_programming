02c91541f10c1588d6f77070b4cdc6c0f1fd2d45
purity kabuga <puritykabuga@gmail.com>
2023-04-25 04:21:16 -0700

    low aphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main prints the alphabet in lowercase using putchar function
+ * Returns: 0 is a succes
+*/
+
+int main(void)
+
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+purchar(ch);
+purchar('\n');
+
+return (0);
+}
