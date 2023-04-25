b9fa867ecf16f7990239384655f20d98ffd2c1c9
purity kabuga <puritykabuga@gmail.com>
2023-04-25 12:51:44 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - prints using function printf
+ * Return: 0 is success
+int main() {
+  printf("_putchar\n");
+   return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+learning 0x02. C - Functions
+learning  nested loops
