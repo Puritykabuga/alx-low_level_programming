d686982e039421aad0e579c5c5b786ae79bea17d
purity kabuga <puritykabuga@gmail.com>
2023-04-25 05:14:27 -0700

    digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 starting from 0 function pintf
+ * Return:0 is a success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
