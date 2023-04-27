6cb83ead49ece2e946c824cf1830557943760413
purity kabuga <puritykabuga@gmail.com>
2023-04-27 00:48:58 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check for uppercase
+ * return: 1 for upper letter or for else is 0
+ */
+
+int int rint _isupper(int c);
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
