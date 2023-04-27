469b2d91245856dda93efe5422ca688735306baa
purity kabuga <puritykabuga@gmail.com>
2023-04-27 03:23:05 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - check for uppercase
- * @y: number to be checked
+ * _isupper - check for upper
+ * @x: number to be checked
  * return: 1 for upper letter or for else is 0
- */
+*/
 
-int _isupper(int y)
+int _isupper(int x)
 
 {
-	if (y >= 65 && y <= 90)
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
