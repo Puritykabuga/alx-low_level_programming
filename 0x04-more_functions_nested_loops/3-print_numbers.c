72d1e159dff7e07ca58a86042732eec8cd3a5a51
purity kabuga <puritykabuga@gmail.com>
2023-04-27 01:54:16 -0700

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main -  prints the numbers using function putchar
+ * Return: numbers 0 upto 9
+ */
+
+void print_numbers(void)
+
+{
+
+int i;
+for (i = 0; i <= 10; i++)
+{
+_putchar(i + '0');
+{
+_putchar(\n");
+}
+return (0);
+}
