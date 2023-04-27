84bb3484f48a7a3aa99637554f65ecac76fb0502
purity kabuga <puritykabuga@gmail.com>
2023-04-27 01:10:17 -0700

    checks digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,15 @@
-#include<ctype.h>
+#include "main.h"
 
 /**
  * main - checks for a digit
  * Return: 1 if c is a digit
  */
 
-
+int _isdigit(int c);
+{
+if (c >=48 && c<=57)
+{
+return (1);
+}
+return (0);
+}
