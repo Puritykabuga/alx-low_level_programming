800b2607acbb944c53391b8fddb3f8639d41a022
purity kabuga <puritykabuga@gmail.com>
2023-04-28 03:41:45 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - Check if a letter is upper
- * @i: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int i)
+int _isupper(int c)
 
 {
 
-	if (isupper(i))
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
