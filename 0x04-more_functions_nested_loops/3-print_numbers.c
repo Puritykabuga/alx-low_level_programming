b8aa064ccb32fa80c2709bb62a2cbac6f4254bd3
purity kabuga <puritykabuga@gmail.com>
2023-04-28 04:16:09 -0700

    prints the number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
+ * Return: 0 is a success
  */
 
 void print_numbers(void)
 
 {
 
-	int x;
+	char c;
 
-	for (x = 0; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(x + '0');
+	_putchar(c);
 	}
 	_putchar('\n');
 }
