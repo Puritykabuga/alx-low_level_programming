42314ff08818fcc28df91403e8b71acd265b8ae8
purity kabuga <puritykabuga@gmail.com>
2023-04-28 14:18:53 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
  * main - testfunction that prints integer is positive or negative
- * Return: 0 is success
+ * Return: 0
  */
 
 int main(void)
 
 {
+
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
