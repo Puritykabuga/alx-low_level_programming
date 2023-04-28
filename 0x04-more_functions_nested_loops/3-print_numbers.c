ac4322ce8a02fd3f4bf1b81d1c3ca5b250f96aef
purity kabuga <puritykabuga@gmail.com>
2023-04-28 03:06:00 -0700

    3 Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
-/*
- * main -  prints the numbers using function putchar
- * Return: numbers 0 upto 9
+
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
 
 {
 
-int c;
-for (c = 0; c <= 9; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-return (0);
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
 }
