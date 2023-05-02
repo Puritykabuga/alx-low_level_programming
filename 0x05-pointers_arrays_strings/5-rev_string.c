0836bf1be904696e1f04bf9881f742271d57f706
purity kabuga <puritykabuga@gmail.com>
2023-05-02 09:58:31 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 
 /**
- * _puts - function that prints string
- * @str: string to print
+ * rev_string - function that reverses a string
+ * @s: string to be reversed
  * Return: void
  */
 
-void rev_string(char *str)
+void rev_string(char *s)
 
 {
-for (; *str != '\0'; str--)
+
+int len = 0, index = 0;
+char tmp;
+while (s[index++])
+len++;
+
+for (index = len - 1; index >= len / 2; index--)
 {
-_putchar(*str);
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
 }
-_putchar('\n');
 }
