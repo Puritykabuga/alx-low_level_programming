8883a7994c1051b9c23102408846b9e47984a572
purity kabuga <puritykabuga@gmail.com>
2023-05-02 10:27:22 -0700

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - function that prints every color followed by a new line
+ * @str: string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+
+{
+int i;
+int j = 0;
+
+while (str[j] != '\0')
+{
+j++;
+}
+
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
