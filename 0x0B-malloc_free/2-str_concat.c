09e3eb846be5e7a15fc7132cd1af257c89b6d247
purity kabuga <puritykabuga@gmail.com>
2023-05-16 12:01:16 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,17 +9,17 @@
  * Return: two strings concatenates
  */
 
-char *str_concat(char *s1, char *s2);
-
+char *str_concat(char *s1, char *s2)
 {
-
-int i = 0, j = 0, l = 0;
+int i = 0, j = 0, k = 0, l = 0;
 char *s;
 
 if (s1 == NULL)
 s1 = "";
+
 if (s2 == NULL)
-S2 = "";
+s2 = "";
+
 while (s1[i])
 i++;
 
@@ -27,7 +27,7 @@ while (s2[j])
 j++;
 
 l = i + j;
-s = malloc((sizeof(chat) * l) +1);
+s = malloc((sizeof(char) * l) +1);
 
 if (s == NULL)
 return (NULL);
