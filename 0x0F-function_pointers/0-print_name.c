db1e3d680876aa1a3d94256642bb4e3e2031690e
purity kabuga <puritykabuga@gmail.com>
2023-05-25 01:08:50 -0700

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
  * @f: pointer to function
  * Return: Nothing.
@@ -11,7 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL ||  f == NULL)
+if (name == NULL || f == NULL)
 return;
 
 f(name);
