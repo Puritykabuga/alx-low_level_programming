7131ad4aa11e9cb11416ff2283f23d81ccf2b949
purity kabuga <puritykabuga@gmail.com>
2023-06-04 02:46:51 -0700

    prints all the elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
-while (h)
+size_t count = 0;
+while (h != NULL)
 {
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%U] %S\n", h->len, h->str);
+printf("'%s'", h->str);
+count++;
 h = h->next;
-s++;
+if (h != NULL)
+printf(" , ");
 }
-return (s);
+printf("]\n");
+
+return (count);
 }
