8b50c0c7028f9fc86818ef8c0db59a2d618e0f1c
purity kabuga <puritykabuga@gmail.com>
2023-06-04 02:14:47 -0700

    returns the number of elements in a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
  * print_list - prints all the elements
- * @list_t - list
  * @h: pointer to the list to print
  * Return: the number of nodes
  */
@@ -15,11 +12,10 @@ size_t print_list(const list_t *h)
 size_t s = 0;
 while (h)
 {
-
 if (!h->str)
 printf("[0] (nil)\n");
 else
-printf("[%U] %s\n", h->len, h->str);
+printf("[%U] %S\n", h->len, h->str);
 h = h->next;
 s++;
 }
