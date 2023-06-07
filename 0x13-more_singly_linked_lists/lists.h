6c3dd8b2ba38d2556cb58aac239da533d35812c6
purity kabuga <puritykabuga@gmail.com>
2023-06-07 00:22:06 -0700

    returns the number of elements in a linked

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,8 @@ typedef struct listint_s
 {
     int n;
     struct listint_s *next;
-} list_t;
-size_t print_listint(const list_t *h);
+} listint_t;
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
