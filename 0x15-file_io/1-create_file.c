914bb832eb20c6ab34e81e94ea614ba1cc923761
purity kabuga <puritykabuga@gmail.com>
2023-06-15 01:43:24 -0700

    copies the content of a file to another file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fp, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fp, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fp == -1 || w == -1)
 		return (-1);
-	close(fd);
+	close(fp);
 
 	return (1);
 }
