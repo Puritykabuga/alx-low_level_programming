14fbfa358784e9155c229879b304f6cbbb1ad46a
purity kabuga <puritykabuga@gmail.com>
2023-06-15 01:34:48 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, w, len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fp, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (fp == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 	close(fp);
 
