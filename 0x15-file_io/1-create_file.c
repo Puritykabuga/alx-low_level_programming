3faad60c01789663c81acb02ceb4868289481657
purity kabuga <puritykabuga@gmail.com>
2023-06-15 01:23:31 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Returns: 1 success, -1 failure (file can not be created, file can not be written, write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fp, text_content, len);
+
+	if (fp == -1 || w == -1)
+		return (-1);
+	close(fp);
+
+	return (1);
+}
